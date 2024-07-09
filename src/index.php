afae8a250f0aa388ca6591a079df30e80d337ea6
starling188 <142456605+starling188@users.noreply.github.com>
2024-07-09 16:38:48 -0400

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicación PHP Docker</title>
+</head>
+<body>
+    <h1>Esta es mi aplicacion en Docker</h1>
+    <p>Starling Jose Gonzalez Miranda 2022-2031</p>
+</body>
+</html>
+
+<?php
